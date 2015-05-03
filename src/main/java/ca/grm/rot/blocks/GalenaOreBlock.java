22aaa13b9da5b876f4876dd7cdeaec0c7ea0f404
Trystan <trystancaffey@gmail.com>
2015-05-02 18:38:03 -0700

    Fixed Galena Ore! It now works!

--- src/main/java/ca/grm/rot/blocks/GalenaOreBlock.java
+++ src/main/java/ca/grm/rot/blocks/GalenaOreBlock.java
@@ -1,12 +1,14 @@
 package ca.grm.rot.blocks;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.init.Items;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 // Only to be used for Galena Ore
 public class GalenaOreBlock extends Block
@@ -16,12 +18,14 @@ public class GalenaOreBlock extends Block
 		super(materialIn);
 	}
 	
-	public ArrayList <ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) 
+	@Override
+	public ArrayList <ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune) 
 	{
 		ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
 		// Define drops here
 		drops.add(new ItemStack(RotBlocks.leadOre, 1));
-		if (world.rand.nextFloat() < 0.5F)
+		Random rand = new Random();
+		if (rand.nextFloat() < 0.5F)
 	        drops.add(new ItemStack(RotBlocks.silverOre));
 		return drops;
     }
