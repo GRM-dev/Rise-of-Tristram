60a867799bd0182e91d88157456edf7aa38f4e9b
Trystan <trystancaffey@gmail.com>
2015-05-18 17:18:33 -0700

    Fixed the retarded push issues.

--- src/main/java/ca/grm/rot/items/RotItems.java
+++ src/main/java/ca/grm/rot/items/RotItems.java
@@ -11,6 +11,7 @@ import ca.grm.rot.Rot;
 public class RotItems {
 	
 	// Armor Materials
+	
 	public static ArmorMaterial QUILTED = EnumHelper.addArmorMaterial("QUILTED", Rot.MOD_ID + ":quiltedArmor", 5, new int[] {1, 2, 3, 1}, 13);
 	public static ArmorMaterial HARDLEATHER = EnumHelper.addArmorMaterial("HARDLEATHER", Rot.MOD_ID + ":hardLeatherArmor", 7, new int[] {1, 2, 3, 1}, 14);
 	public static ArmorMaterial STUDDED = EnumHelper.addArmorMaterial("STUDDED", Rot.MOD_ID + ":studdedArmor", 9, new int[] {1, 2, 3, 1}, 13);
@@ -127,8 +128,8 @@ public class RotItems {
 	// Ancient Armor
 	public static Item ancientArmorHelm;
 	public static Item ancientArmorChestplate;
-	public static Item ancientPlateArmorLeggings;
-	public static Item ancientPlateArmorBoots;
+	public static Item ancientArmorLeggings;
+	public static Item ancientArmorBoots;
 	
 	public static void init()
 	{
@@ -205,6 +206,36 @@ public class RotItems {
 		lightPlateArmorChestplate = new RotArmors("lightPlateArmorChestplate", LIGHTPLATE, 2, 1).setCreativeTab(Rot.tabRot);
 		lightPlateArmorLeggings = new RotArmors("lightPlateArmorLeggings", LIGHTPLATE, 1, 2).setCreativeTab(Rot.tabRot);
 		lightPlateArmorBoots = new RotArmors("lightPlateArmorBoots", LIGHTPLATE, 1, 3).setCreativeTab(Rot.tabRot);
+		
+		//// Field Plate Armor
+		fieldPlateArmorHelm = new RotArmors("fieldPlateArmorHelm", FIELDPLATE, 1, 0).setCreativeTab(Rot.tabRot);
+		fieldPlateArmorChestplate = new RotArmors("fieldPlateArmorChestplate", FIELDPLATE, 2, 1).setCreativeTab(Rot.tabRot);
+		fieldPlateArmorLeggings = new RotArmors("fieldPlateArmorLeggings", FIELDPLATE, 1, 2).setCreativeTab(Rot.tabRot);
+		fieldPlateArmorBoots = new RotArmors("fieldPlateArmorBoots", FIELDPLATE, 1, 3).setCreativeTab(Rot.tabRot);
+		
+		//// Plate Mail Armor
+		plateMailArmorHelm = new RotArmors("plateMailArmorHelm", PLATEMAIL, 1, 0).setCreativeTab(Rot.tabRot);
+		plateMailArmorChestplate = new RotArmors("plateMailArmorChestplate", PLATEMAIL, 2, 1).setCreativeTab(Rot.tabRot);
+		plateMailArmorLeggings = new RotArmors("plateMailArmorLeggings", PLATEMAIL, 1, 2).setCreativeTab(Rot.tabRot);
+		plateMailArmorBoots = new RotArmors("plateMailArmorBoots", PLATEMAIL, 1, 3).setCreativeTab(Rot.tabRot);
+		
+		//// Gothic Plate Armor
+		gothicPlateArmorHelm = new RotArmors("gothicPlateArmorHelm", GOTHICPLATE, 1, 0).setCreativeTab(Rot.tabRot);
+		gothicPlateArmorChestplate = new RotArmors("gothicPlateArmorChestplate", GOTHICPLATE, 2, 1).setCreativeTab(Rot.tabRot);
+		gothicPlateArmorLeggings = new RotArmors("gothicPlateArmorLeggings", GOTHICPLATE, 1, 2).setCreativeTab(Rot.tabRot);
+		gothicPlateArmorBoots = new RotArmors("gothicPlateArmorBoots", GOTHICPLATE, 1, 3).setCreativeTab(Rot.tabRot);
+		
+		//// Full Plate Armor
+		fullPlateArmorHelm = new RotArmors("fullPlateArmorHelm", FULLPLATE, 1, 0).setCreativeTab(Rot.tabRot);
+		fullPlateArmorChestplate = new RotArmors("fullPlateArmorChestplate", FULLPLATE, 2, 1).setCreativeTab(Rot.tabRot);
+		fullPlateArmorLeggings = new RotArmors("fullPlateArmorLeggings", FULLPLATE, 1, 2).setCreativeTab(Rot.tabRot);
+		fullPlateArmorBoots = new RotArmors("fullPlateArmorBoots", FULLPLATE, 1, 3).setCreativeTab(Rot.tabRot);
+		
+		//// Ancient Armor
+		ancientArmorHelm = new RotArmors("ancientArmorHelm", ANCIENT, 1, 0).setCreativeTab(Rot.tabRot);
+		ancientArmorChestplate = new RotArmors("ancientArmorChestplate", ANCIENT, 2, 1).setCreativeTab(Rot.tabRot);
+		ancientArmorLeggings = new RotArmors("ancientArmorLeggings", ANCIENT, 1, 2).setCreativeTab(Rot.tabRot);
+		ancientArmorBoots = new RotArmors("ancientArmorBoots", ANCIENT, 1, 3).setCreativeTab(Rot.tabRot);
 	}
 	
 	public static void register()
@@ -255,6 +286,46 @@ public class RotItems {
 		GameRegistry.registerItem(scaleMailArmorChestplate, scaleMailArmorChestplate.getUnlocalizedName().substring(5));
 		GameRegistry.registerItem(scaleMailArmorLeggings, scaleMailArmorLeggings.getUnlocalizedName().substring(5));
 		GameRegistry.registerItem(scaleMailArmorBoots, scaleMailArmorBoots.getUnlocalizedName().substring(5));
+		
+		GameRegistry.registerItem(breastPlateArmorHelm, breastPlateArmorHelm.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(breastPlateArmorChestplate, breastPlateArmorChestplate.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(breastPlateArmorLeggings, breastPlateArmorLeggings.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(breastPlateArmorBoots, breastPlateArmorBoots.getUnlocalizedName().substring(5));
+		
+		GameRegistry.registerItem(splintMailArmorHelm, splintMailArmorHelm.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(splintMailArmorChestplate, splintMailArmorChestplate.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(splintMailArmorLeggings, splintMailArmorLeggings.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(splintMailArmorBoots, splintMailArmorBoots.getUnlocalizedName().substring(5));
+		
+		GameRegistry.registerItem(lightPlateArmorHelm, lightPlateArmorHelm.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(lightPlateArmorChestplate, lightPlateArmorChestplate.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(lightPlateArmorLeggings, lightPlateArmorLeggings.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(lightPlateArmorBoots, lightPlateArmorBoots.getUnlocalizedName().substring(5));
+		
+		GameRegistry.registerItem(fieldPlateArmorHelm, fieldPlateArmorHelm.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(fieldPlateArmorChestplate, fieldPlateArmorChestplate.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(fieldPlateArmorLeggings, fieldPlateArmorLeggings.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(fieldPlateArmorBoots, fieldPlateArmorBoots.getUnlocalizedName().substring(5));
+		
+		GameRegistry.registerItem(plateMailArmorHelm, plateMailArmorHelm.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(plateMailArmorChestplate, plateMailArmorChestplate.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(plateMailArmorLeggings, plateMailArmorLeggings.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(plateMailArmorBoots, plateMailArmorBoots.getUnlocalizedName().substring(5));
+		
+		GameRegistry.registerItem(gothicPlateArmorHelm, gothicPlateArmorHelm.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(gothicPlateArmorChestplate, gothicPlateArmorChestplate.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(gothicPlateArmorLeggings, gothicPlateArmorLeggings.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(gothicPlateArmorBoots, gothicPlateArmorBoots.getUnlocalizedName().substring(5));
+		
+		GameRegistry.registerItem(fullPlateArmorHelm, fullPlateArmorHelm.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(fullPlateArmorChestplate, fullPlateArmorChestplate.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(fullPlateArmorLeggings, fullPlateArmorLeggings.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(fullPlateArmorBoots, fullPlateArmorBoots.getUnlocalizedName().substring(5));
+		
+		GameRegistry.registerItem(ancientArmorHelm, ancientArmorHelm.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(ancientArmorChestplate, ancientArmorChestplate.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(ancientArmorLeggings, ancientArmorLeggings.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(ancientArmorBoots, ancientArmorBoots.getUnlocalizedName().substring(5));
 	}
 	
 	public static void registerRenders()
@@ -305,6 +376,46 @@ public class RotItems {
 		registerRender(scaleMailArmorChestplate);
 		registerRender(scaleMailArmorLeggings);
 		registerRender(scaleMailArmorBoots);
+		
+		registerRender(breastPlateArmorHelm);
+		registerRender(breastPlateArmorChestplate);
+		registerRender(breastPlateArmorLeggings);
+		registerRender(breastPlateArmorBoots);
+		
+		registerRender(splintMailArmorHelm);
+		registerRender(splintMailArmorChestplate);
+		registerRender(splintMailArmorLeggings);
+		registerRender(splintMailArmorBoots);
+		
+		registerRender(lightPlateArmorHelm);
+		registerRender(lightPlateArmorChestplate);
+		registerRender(lightPlateArmorLeggings);
+		registerRender(lightPlateArmorBoots);
+		
+		registerRender(fieldPlateArmorHelm);
+		registerRender(fieldPlateArmorChestplate);
+		registerRender(fieldPlateArmorLeggings);
+		registerRender(fieldPlateArmorBoots);
+		
+		registerRender(plateMailArmorHelm);
+		registerRender(plateMailArmorChestplate);
+		registerRender(plateMailArmorLeggings);
+		registerRender(plateMailArmorBoots);
+		
+		registerRender(gothicPlateArmorHelm);
+		registerRender(gothicPlateArmorChestplate);
+		registerRender(gothicPlateArmorLeggings);
+		registerRender(gothicPlateArmorBoots);
+		
+		registerRender(fullPlateArmorHelm);
+		registerRender(fullPlateArmorChestplate);
+		registerRender(fullPlateArmorLeggings);
+		registerRender(fullPlateArmorBoots);
+		
+		registerRender(ancientArmorHelm);
+		registerRender(ancientArmorChestplate);
+		registerRender(ancientArmorLeggings);
+		registerRender(ancientArmorBoots);
 	}
 	
 	public static void registerRender(Item item)
