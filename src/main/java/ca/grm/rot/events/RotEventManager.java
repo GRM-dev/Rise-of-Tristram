edb48317f7c7b86adf672f47e42764dd89e22705
Trystan <trystancaffey@gmail.com>
2015-05-20 19:35:19 -0700

    I've got a semitransparent square to render! Progress! Needs to rotate still...

--- src/main/java/ca/grm/rot/events/RotEventManager.java
+++ src/main/java/ca/grm/rot/events/RotEventManager.java
@@ -2,8 +2,6 @@ package ca.grm.rot.events;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.common.event.FMLEvent;
-import net.minecraftforge.fml.common.network.FMLEventChannel;
 
 public class RotEventManager 
 {
@@ -19,6 +17,7 @@ public class RotEventManager
 		MinecraftForge.EVENT_BUS.register(new RotEventItems());
 		MinecraftForge.EVENT_BUS.register(new RotEventPlayerClone());
 		MinecraftForge.EVENT_BUS.register(new RotEventTeamsClasses());
+		MinecraftForge.EVENT_BUS.register(new RotEventRenderLivingEvent());
 		FMLCommonHandler.instance().bus().register(new RotFMLEventItems());
 	}
 }
--- src/main/java/ca/grm/rot/events/RotEventRenderLivingEvent.java
+++ src/main/java/ca/grm/rot/events/RotEventRenderLivingEvent.java
@@ -1,27 +1,32 @@
 package ca.grm.rot.events;
 
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.entity.Entity;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public abstract class RotEventRenderLivingEvent {
-	public void drawNameWithHealth()
+import org.lwjgl.opengl.GL11;
+
+public class RotEventRenderLivingEvent {
+	public void drawNameWithHealth(Entity entity)
 	{
 		Tessellator t = Tessellator.getInstance(); 
 		WorldRenderer tw = t.getWorldRenderer();
 		tw.startDrawingQuads();
+		GL11.glEnable(GL11.GL_BLEND);
 		tw.setColorRGBA_F(0.0F, 0.0F, 0.0F, 0.25F);
-		tw.addVertex(0,0,0);
-		tw.addVertex(0,1,0);
-		tw.addVertex(1,1,0);
-		tw.addVertex(1,0,0);
+		tw.addVertex(0,2,0);
+		tw.addVertex(0,3,0);
+		tw.addVertex(1,3,0);
+		tw.addVertex(1,2,0);
 		t.draw(); 
 	}
 	
 	@SubscribeEvent
 	public void handlerRenderLiving(RenderLivingEvent.Pre event)
 	{
-		drawNameWithHealth();
+		drawNameWithHealth(event.entity);
 	}
 }
